b6ec279e280febbabcf390e07c20dd867b28d0f3
Daniel Weinmann <danielweinmann@gmail.com>
2014-12-17 14:47:00 -0200

    Making facebook image large

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,7 +2,7 @@ module UserDecorator
 
   def display_image
     if self.image.present?
-      image_tag(self.image)
+      image_tag("#{self.image}?type=large")
     else
       image_tag('user.svg')
     end

49a3135ea7c920ce3438b5f42ca0393063cda92d
Daniel Weinmann <danielweinmann@gmail.com>
2014-12-21 12:49:06 -0200

    Removes unexpected raise debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ActiveRecord::Base
   before_save :clean_permalink
 
   def self.from_omniauth(auth)
-    raise auth.uid.inspect
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.name = auth.info.name
